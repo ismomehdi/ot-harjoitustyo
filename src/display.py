e8c141d1638d67431cd31c7e042c13ce62eddfe7
Ismo Mehdi <84803085+ismomehdi@users.noreply.github.com>
2022-11-22 19:43:58 +0200

    Update minor changes

---
+++ src/display.py
@@ -0,0 +1,10 @@
+import pygame
+
+display_width = 1280
+display_height = 720
+
+display = pygame.display.set_mode((display_width, display_height))
+display_surface = pygame.display.get_surface()
+
+pygame.display.set_caption("The Adventures of Yakuzi")
+
