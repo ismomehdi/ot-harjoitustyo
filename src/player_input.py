5307a7ac38b51e058ff2fe41e9d1460d4ecc5c55
Ismo Mehdi <84803085+ismomehdi@users.noreply.github.com>
2022-11-24 00:35:33 +0200

    Add player_input

---
+++ src/player_input.py
@@ -0,0 +1,21 @@
+import pygame
+
+def player_input(direction, jump_speed, player_on_ground):
+    keys = pygame.key.get_pressed()
+
+    # This controls the player left and right movement
+    if keys[pygame.K_RIGHT]:
+        direction.x = 1
+    elif keys[pygame.K_LEFT]:
+        direction.x = -1
+    else:
+        direction.x = 0
+
+    # This controls the player jump
+    if keys[pygame.K_UP] and player_on_ground:
+        direction.y = -jump_speed
+    
+    
+    
+
+    
