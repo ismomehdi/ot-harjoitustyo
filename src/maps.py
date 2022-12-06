d6dd5122269c67c4dc67fab31d77d616aa02225e
Ismo Mehdi <84803085+ismomehdi@users.noreply.github.com>
2022-12-06 15:15:28 +0200

    Add .txt file levels

--- src/maps.py
+++ src/maps.py
@@ -1,39 +1,24 @@
+from os import walk
+
+# The level maps are saved as .txt files in the levels folder.
+# The following characters are used to build the level:
+#
 # P = Player
 # X = Ground tile
 # x = Sky tile
 # o = Coin
 
 TILE_SIZE = 64
+path = 'src/levels/'
+maps = {}
 
-level_1 = [
-    '                                                           ',
-    '                                                           ',
-    '                                                           ',
-    '                                                           ',
-    '                                                           ',
-    '                                                           ',
-    '                                                           ',
-    '                                                           ',
-    '                                                           ',
-    '                                                           ',
-    '                                                           ',
-    '                                                           ',
-    '                                                           ',
-    '                                                           ',
-    '                                                           ',
-    '                                                           ',
-    '                                                           ',
-    '                                                           ',
-    '                                                           ',
-    '                                                           ',
-    '                                                           ',
-    '                                                           ',
-    '                                                           ',
-    '                                                           ',
-    '                                                           ',
-    '          P     ooooooooooooo                              ',
-    'XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX']
+for _, __, filenames in walk(path):
+    for filename in sorted(filenames):
+        level_file = open(path + filename, 'r')
+        data = level_file.read().replace("'", "").split("\n")
+        filename = filename.strip('.txt')
+        maps[filename] = data
+        level_file.close()
 
-maps = {
-    'level_1': level_1
-}
+
+        
