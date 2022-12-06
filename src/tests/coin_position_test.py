516307d8914fb1e96c9861b4e2490e541bef7a62
Ismo Mehdi <84803085+ismomehdi@users.noreply.github.com>
2022-12-06 22:54:25 +0200

    Add coin position test

---
+++ src/tests/coin_position_test.py
@@ -0,0 +1,36 @@
+import unittest
+from world import World
+from maps import TILE_SIZE
+from display import display_surface
+
+level_map_0 = [
+    '                                                        ',
+    '                                                        ',
+    '                                                        ',
+    '                                                        ',
+    '                                                        ',
+    '                                                        ',
+    '                                                        ',
+    '                                                        ',
+    '                                                        ',
+    '                                                        ',
+    ' o                                                      ',
+    'XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX',
+    'XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX',
+    'XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX']
+
+
+class TestWorld(unittest.TestCase):
+    def setUp(self):
+        self.world = World(level_map_0, display_surface)
+
+    def assert_coordinates_equal(self, sprite, x):
+        self.assertEqual(sprite.rect.x, x)
+
+    def test_coin_position(self):
+        coins = self.world.coin_sprites
+
+        for coin in coins:
+            self.assert_coordinates_equal(coin, TILE_SIZE)
+
+        
