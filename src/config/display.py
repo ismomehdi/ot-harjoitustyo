39764bebb8b2353c1b24efb806e57b06e711aa19
Ismo Mehdi <84803085+ismomehdi@users.noreply.github.com>
2022-12-09 02:16:20 +0200

    Add config

---
+++ src/config/display.py
@@ -0,0 +1,9 @@
+import pygame
+
+DISPLAY_WIDTH = 1280
+DISPLAY_HEIGHT = 720
+
+display = pygame.display.set_mode((DISPLAY_WIDTH, DISPLAY_HEIGHT))
+display_surface = pygame.display.get_surface()
+
+pygame.display.set_caption("The Adventures of Yakuzi")
---
+++ src/config/sprite_sizes.py
@@ -0,0 +1,8 @@
+# Most of the sprites are fixed size or 
+# they are scaled to the tile size.
+
+TILE_SIZE = 48
+
+# The enemy size offset is used to make the enemy 
+# sprite appear on the correct position.
+ENEMY_SIZE_OFFSET = 27
